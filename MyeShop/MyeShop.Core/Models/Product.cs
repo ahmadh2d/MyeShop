676ab4e641f96e002d15d13e4aca6363cb1dd72b
ahmadh2d <38376104+ahmadh2d@users.noreply.github.com>
2020-08-08 13:32:41 +0500

    Product Model and InMemory Product Repository Build

---
+++ MyeShop/MyeShop.Core/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyeShop.Core.Models
+{
+	public class Product
+	{
+		public string Id { get; set; }
+		[StringLength(20)]
+		[DisplayName("Product Name")]
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		[Range(0, 1000)]
+		public decimal Price { get; set; }
+		public string Category { get; set; }
+		public string Image { get; set; }
+
+		public Product()
+		{
+			this.Id = Guid.NewGuid().ToString();
+		}
+	}
+}
