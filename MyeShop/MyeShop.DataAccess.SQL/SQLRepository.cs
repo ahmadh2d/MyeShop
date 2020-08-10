5cfdf29725d8fcae4a33bae7a7e08a5dc5e03f63
ahmadh2d <38376104+ahmadh2d@users.noreply.github.com>
2020-08-10 18:03:02 +0500

    Implemented SQL Repository

---
+++ MyeShop/MyeShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,56 @@
+﻿using MyeShop.Core.Contracts;
+using MyeShop.Core.Models;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MyeShop.DataAccess.SQL
+{
+	public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+	{
+		internal DataContext context;
+		internal DbSet<T> dbSet;
+
+		public SQLRepository(DataContext context)
+		{
+			this.context = context;
+			this.dbSet = context.Set<T>();
+		}
+
+		public IQueryable<T> Collection()
+		{
+			return dbSet;
+		}
+
+		public void Commit()
+		{
+			context.SaveChanges();
+		}
+
+		public void Delete(string id)
+		{
+			var item = this.Find(id);
+			if (context.Entry(item).State == EntityState.Detached)
+			{
+				dbSet.Attach(item);
+			}
+
+			dbSet.Remove(item);
+		}
+
+		public T Find(string id)
+		{
+			return dbSet.Find(id);
+		}
+
+		public void Insert(T item)
+		{
+			dbSet.Add(item);
+		}
+
+		public void Update(T item)
+		{
+			dbSet.Attach(item);
+			context.Entry(item).State = EntityState.Modified;
+		}
+	}
+}
--- MyeShop/MyeShop.WebUI/App_Start/UnityConfig.cs
+++ MyeShop/MyeShop.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyeShop.Core.Contracts;
 using MyeShop.Core.Models;
 using MyeShop.DataAccess.InMemory;
+using MyeShop.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyeShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
